590cbc8cb90dd8fd468bb6b3187d157fd1bd7e54
Akos Kemives <akoskm@gmail.com>
2016-05-15 10:04:14 +0200

    separate toolbar component

--- src/components/photo/Photos.js
+++ src/components/photo/Photos.js
@@ -3,14 +3,9 @@ import $ from 'jquery';
 
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-import ControlLabel from 'react-bootstrap/lib/ControlLabel';
-import Button from 'react-bootstrap/lib/Button';
-import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
-import Form from 'react-bootstrap/lib/Form';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
-import FormControl from 'react-bootstrap/lib/FormControl';
 
 import Photo from './Photo';
+import Toolbar from './Toolbar';
 
 class Photos extends React.Component {
 
@@ -25,14 +20,10 @@ class Photos extends React.Component {
 
     this.onDeleteClick = this.onDeleteClick.bind(this);
     this.onPhotoSelect = this.onPhotoSelect.bind(this);
-    this.onCreateAlbum = this.onCreateAlbum.bind(this);
-    this.handleAlbumNameChange = this.handleAlbumNameChange.bind(this);
-    this.handleScroll = this.handleScroll.bind(this);
   }
 
   componentDidMount() {
     const nameslug = this.props.nameslug;
-    window.addEventListener('scroll', this.handleScroll);
     this.serverRequest = $.get('/api/pages/' + nameslug + '/photos', (response) => {
       const data = response.result;
       if (response.success && data) {
@@ -46,12 +37,6 @@ class Photos extends React.Component {
 
   componentWillUnmount() {
     this.serverRequest.abort();
-  }
-
-  onCreateAlbum() {
-    this.setState({
-      newAlbumName: 'Untitled Album'
-    });
   }
 
   onDeleteClick(photoid, index) {
@@ -87,65 +72,16 @@ class Photos extends React.Component {
     });
   }
 
-  handleScroll() {
-    let scrollTop = event.srcElement.body.scrollTop;
-    if (scrollTop > 220) {
-      $('div.create-album-toolbar').addClass('fixed-top container');
-    } else {
-      $('div.create-album-toolbar').removeClass('fixed-top container');
-    }
-  }
-
-  handleAlbumNameChange(e) {
-    this.setState({
-      newAlbumName: e.target.value
-    });
-  }
-
   render() {
     const photos = this.state.photos;
     const nameslug = this.props.nameslug;
     const selectedPhotos = this.state.selected;
-    let createAlbumText;
-    let selectedCount = null;
-    let creatingAlbum = false;
-    let newAlbumButton = '';
-    if (selectedPhotos) {
-      selectedCount = selectedPhotos.length;
-      if (selectedCount === 1) {
-        // createAlbumDisabled = false;
-        createAlbumText = 'Selected 1 photo.';
-      } else if (selectedCount > 1) {
-        createAlbumText = 'Selected ' + selectedCount + ' photos.';
-      }
-    }
-    if (this.state.newAlbumName) {
-      creatingAlbum = true;
-      newAlbumButton = (
-        <FormGroup>
-          <ControlLabel>How you would like to call it?</ControlLabel>
-          {' '}
-          <FormControl value={this.state.newAlbumName} onChange={this.handleAlbumNameChange}/>
-          {' '}
-          <Button
-            className='btn btn-success'
-            role='button'
-            type='button'
-          >Ok</Button>
-          {' '}
-          <Button
-            className='btn btn-warning'
-            role='button'
-            type='button'
-          >Cancel</Button>
-        </FormGroup>
-      );
-    }
+
     let markup;
     if (photos && photos.length > 0) {
       markup = photos.map((image, i) => {
         let selected = '';
-        if (creatingAlbum || selectedPhotos && selectedPhotos.length > 0) {
+        if (selectedPhotos && selectedPhotos.length > 0) {
           if (selectedPhotos.indexOf(i) > -1) {
             selected = 'selected';
           } else {
@@ -170,27 +106,7 @@ class Photos extends React.Component {
     }
     return (
       <div>
-        <Row className='create-album-toolbar'>
-          <Col md={12}>
-            <FormGroup>
-              <Form inline>
-                <Button
-                  className='btn btn-primary'
-                  disabled={creatingAlbum}
-                  onClick={this.onCreateAlbum}
-                >
-                  Create Album
-                </Button>
-                {' '}
-                {createAlbumText}
-                {' '}
-                <div className='pull-right'>
-                  {newAlbumButton}
-                </div>
-              </Form>
-            </FormGroup>
-          </Col>
-        </Row>
+        <Toolbar selectedPhotos={this.state.selected}/>
         <Row>
           {markup}
         </Row>
---
+++ src/components/photo/Toolbar.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import $ from 'jquery';
+
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import Button from 'react-bootstrap/lib/Button';
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
+import Form from 'react-bootstrap/lib/Form';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FormControl from 'react-bootstrap/lib/FormControl';
+
+import Photo from './Photo';
+
+class Toolbar extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newAlbumName: ''
+    };
+
+    this.onCreateAlbum = this.onCreateAlbum.bind(this);
+    this.handleAlbumNameChange = this.handleAlbumNameChange.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  onCreateAlbum() {
+    this.setState({
+      newAlbumName: 'Untitled Album'
+    });
+  }
+
+  handleAlbumNameChange(e) {
+    this.setState({
+      newAlbumName: e.target.value
+    });
+  }
+
+  handleScroll() {
+    let scrollTop = event.srcElement.body.scrollTop;
+    if (scrollTop > 220) {
+      $('div.create-album-toolbar').addClass('fixed-top container');
+    } else {
+      $('div.create-album-toolbar').removeClass('fixed-top container');
+    }
+  }
+
+  render() {
+    let creatingAlbum = false;
+    let newAlbumButton = '';
+    let createAlbumText;
+    let selectedCount = null;
+
+    const selectedPhotos = this.props.selectedPhotos;
+    if (selectedPhotos) {
+      selectedCount = selectedPhotos.length;
+      if (selectedCount === 1) {
+        // createAlbumDisabled = false;
+        createAlbumText = 'Selected 1 photo.';
+      } else if (selectedCount > 1) {
+        createAlbumText = 'Selected ' + selectedCount + ' photos.';
+      }
+    }
+    if (this.state.newAlbumName) {
+      creatingAlbum = true;
+      newAlbumButton = (
+        <FormGroup>
+          <ControlLabel>How you would like to call it?</ControlLabel>
+          {' '}
+          <FormControl value={this.state.newAlbumName} onChange={this.handleAlbumNameChange}/>
+          {' '}
+          <Button
+            className='btn btn-success'
+            role='button'
+            type='button'
+          >Ok</Button>
+          {' '}
+          <Button
+            className='btn btn-warning'
+            role='button'
+            type='button'
+          >Cancel</Button>
+        </FormGroup>
+      );
+    }
+    return (
+      <Row className='create-album-toolbar'>
+        <Col md={12}>
+          <FormGroup>
+            <Form inline>
+              <Button
+                className='btn btn-primary'
+                disabled={creatingAlbum}
+                onClick={this.onCreateAlbum}
+              >
+                Create Album
+              </Button>
+              {' '}
+              {createAlbumText}
+              {' '}
+              <div className='pull-right'>
+                {newAlbumButton}
+              </div>
+            </Form>
+          </FormGroup>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+Toolbar.propTypes = {
+  selectedPhotos: React.PropTypes.object.isRequired
+};
+
+export default Toolbar;
