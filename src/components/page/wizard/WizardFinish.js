262d531ab8d90f33b6e732d85d6dd6dcf801f7fc
Akos Kemives <akoskm@gmail.com>
2016-08-10 23:20:36 +0200

    make last step a functional component

--- src/components/page/wizard/WizardFinish.js
+++ src/components/page/wizard/WizardFinish.js
@@ -3,22 +3,16 @@ import React from 'react';
 import Alert from 'react-bootstrap/lib/Alert';
 import Button from 'react-bootstrap/lib/Button';
 
-class WizardResult extends React.Component {
-
-  render() {
-    return (
-      <Alert bsStyle='success'>
-          <h4>Success!</h4>
-          <p>{this.props.page.name} has been created.</p>
-          <p>
-            <Button bsStyle='primary' href={this.props.url}>Show me</Button>
-            <span> or </span>
-            <Button onClick={this.props.closeWizard}>Close this wizard</Button>
-          </p>
-        </Alert>
-    );
-  }
-}
+const WizardResult = (props) =>
+  <Alert bsStyle='success'>
+    <h4>Success!</h4>
+    <p>{props.page.name} has been created.</p>
+    <p>
+      <Button bsStyle='primary' href={props.url}>Show me</Button>
+      <span> or </span>
+      <Button onClick={props.closeWizard}>Close this wizard</Button>
+    </p>
+  </Alert>;
 
 WizardResult.propTypes = {
   closeWizard: React.PropTypes.object.isRequired,
