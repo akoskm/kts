f549b23c9639e59517b7ac7c89798251cec77e49
Akos Kemives <akoskm@gmail.com>
2016-12-02 10:15:56 +0100

    signin page alerts and validations

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import $ from 'jquery';
-import { Navigation } from 'react-router';
 import { dispatch } from '../stores/user/UserDispatcher';
 
 import HelpBlock from 'react-bootstrap/lib/HelpBlock';
@@ -8,6 +7,7 @@ import FormControl from 'react-bootstrap/lib/FormControl';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import Button from 'react-bootstrap/lib/Button';
+import Alert from 'react-bootstrap/lib/Alert';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 
@@ -17,7 +17,10 @@ class SignInComponent extends React.Component {
     super(props, context);
     this.state = {
       email: '',
-      passw: ''
+      passw: '',
+      emailSet: '',
+      passwSet: '',
+      errorMessage: ''
     };
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePassChange = this.handlePassChange.bind(this);
@@ -37,18 +40,14 @@ class SignInComponent extends React.Component {
   }
 
   handleSubmit(e) {
-    let self = this;
     e.preventDefault();
-    if (!this.state.email) {
-      this.state.emailError = 'Username is required';
-      if (!this.state.passw) {
-        this.state.passwError = 'Password is required';
-      }
-    } else {
-      $.post('/api/login', this.state).done(function (data) {
+    let emailSet = !!this.state.email;
+    let passwSet = !!this.state.passw;
+    if (emailSet && passwSet) {
+      $.post('/api/login', this.state).done((data) => {
         if (data.success) {
           let user = data.user;
-          self.setState({
+          this.setState({
             username: user.username,
             status: user.status
           });
@@ -56,36 +55,64 @@ class SignInComponent extends React.Component {
             type: 'user/login',
             user
           });
-          self.props.history.pushState(null, '/profile');
+          this.props.history.pushState(null, '/profile');
+        } else {
+          this.setState({
+            errorMessage: data.message
+          });
         }
       });
     }
+    this.setState({
+      emailSet,
+      passwSet,
+      errorMessage: ''
+    });
   }
 
   render() {
+    let emailHelp;
+    let passwHelp;
+    let emailErrorClass;
+    let passwErrorClass;
+    let alert;
+    if (this.state.emailSet === false) {
+      emailHelp = <HelpBlock>Username is required</HelpBlock>;
+      emailErrorClass = 'error';
+    }
+    if (this.state.passwSet === false) {
+      passwHelp = <HelpBlock>Password is required</HelpBlock>;
+      passwErrorClass = 'error';
+    }
+    if (this.state.errorMessage) {
+      alert = (<Alert bsStyle='danger'>
+        <p>{this.state.errorMessage}</p>
+      </Alert>);
+    }
     return (
       <Row>
         <Col md={3}>
-          <form className='registrationForm' onSubmit={this.handleSubmit}>
-            <FormGroup controlId='email' validationState={!this.state.emailError}>
+          <form className='registrationForm'>
+            <FormGroup controlId='email' validationState={emailErrorClass}>
               <ControlLabel>Username</ControlLabel>
               <FormControl
                 type='text'
                 value={this.state.email}
                 onChange={this.handleEmailChange}
               />
-              <HelpBlock>{this.state.emailError}</HelpBlock>
+              {emailHelp}
             </FormGroup>
-            <FormGroup controlId='passw' validationState={!this.state.passwError}>
+            <FormGroup controlId='passw' validationState={passwErrorClass}>
               <ControlLabel>Password</ControlLabel>
               <FormControl
                 type='password'
                 value={this.state.passw}
                 onChange={this.handlePassChange}
               />
-              <HelpBlock>{this.state.passwError}</HelpBlock>
+              {passwHelp}
             </FormGroup>
-            <Button type='submit' secondary>Log in</Button>
+            {alert}
+            <Button type='submit' onClick={this.handleSubmit}>Log in</Button>
           </form>
         </Col>
       </Row>
