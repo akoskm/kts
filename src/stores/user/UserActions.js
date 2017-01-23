2c161476b8cfcb62870e0562f124c191849def1d
Akos Kemives <akoskm@gmail.com>
2017-01-23 11:07:26 +0100

    introduce action types

--- src/stores/user/UserActions.js
+++ src/stores/user/UserActions.js
@@ -1,9 +1,22 @@
-export type Action =
-{
-  type: 'user/login',
-  user: {}
-} |
-{
-  type: 'user/logout',
-  user: {}
+'use strict';
+
+import UserActionTypes from './UserActionTypes';
+import UserDispatcher from './UserDispatcher';
+
+const Actions = {
+  logIn(user) {
+    UserDispatcher.dispatch({
+      type: UserActionTypes.LOGIN,
+      user
+    });
+  },
+
+  logOut(user) {
+    UserDispatcher.dispatch({
+      type: UserActionTypes.LOGOUT,
+      user
+    });
+  }
 };
+
+export default Actions;
